4124ab7da071664c836f6d7ace645216b83fe4e1
dictator_zx <ivanandrosovv@gmail.com>
2022-06-28 17:19:17 +0300

    Initial commit

---
+++ src/main/java/com/androsov/pinboard/PinboardApplication.java
@@ -0,0 +1,13 @@
+package com.androsov.pinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class PinboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinboardApplication.class, args);
+    }
+
+}
