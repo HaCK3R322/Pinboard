8799ac91fa572161a0bc3f8042b8e2d56fc4fec6
dictator_zx <ivanandrosovv@gmail.com>
2022-08-22 19:46:06 +0300

    Deleting to work while I cannot access my notebook

--- src/frontend/src/components/Pin/DeleteDrop/DeleteDrop.jsx
+++ src/frontend/src/components/Pin/DeleteDrop/DeleteDrop.jsx
@@ -3,18 +3,27 @@ import cl from './DeleteDrop.module.css';
 
 import { useDrop } from 'react-dnd'
 
-function DeleteDrop({isVisible, onDrop}) {
+function DeleteDrop({isVisible, onDrop, setIsOver}) {
     const rootClass = [cl.DeleteDrop];
     if (isVisible) {
         rootClass.push(cl.active);
     }
 
-    const [collectedProps, drop] = useDrop(() => ({
+    const [{isOver}, drop] = useDrop(() => ({
         accept: 'PIN',
         drop: () => {
             onDrop();
-        }
+        },
+        collect: monitor => ({
+            isOver: !!monitor.isOver(),
+        })
     }))
+
+    if(isOver) {
+        setIsOver(true);
+    } else {
+        setIsOver(false);
+    }
 
     return <div className={rootClass.join(' ')} ref={drop}></div>
 }
--- src/frontend/src/components/Pin/DoneDrop/DoneDrop.jsx
+++ src/frontend/src/components/Pin/DoneDrop/DoneDrop.jsx
@@ -3,18 +3,27 @@ import cl from './DoneDrop.module.css';
 
 import { useDrop } from 'react-dnd'
 
-function DoneDrop({isVisible, onDrop}) {
+function DoneDrop({isVisible, onDrop, setIsOver}) {
     const rootClass = [cl.DoneDrop];
     if (isVisible) {
         rootClass.push(cl.active);
     }
 
-    const [collectedProps, drop] = useDrop(() => ({
+    const [{isOver}, drop] = useDrop(() => ({
         accept: 'PIN',
         drop: () => {
             onDrop();
-        }
+        },
+        collect: monitor => ({
+            isOver: !!monitor.isOver(),
+        })
     }))
+
+    if(isOver) {
+        setIsOver(true);
+    } else {
+        setIsOver(false);
+    }
 
     return <div className={rootClass.join(' ')} ref={drop}></div>
 }
